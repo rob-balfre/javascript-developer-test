9d072c05168f1417a1698154b1b9908694673dc2
Rob Balfre <robert.balfre@travelctm.com.au>
2021-04-28 07:13:38 +1000

    implementing getArnieQuotes() function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,21 @@
 const { httpGet } = require('./mock-http-interface');
 
+const illBeBack = async (url) => {
+  const response = await httpGet(url);
+  const key = response.status === 200 ? 'Arnie Quote' : 'FAILURE'
+  const { message } = JSON.parse(response.body);
+
+  return {
+    [key]: message
+  };
+};
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const arniePromises = urls.map(async (url) => await illBeBack(url));
+
+  return await Promise.all(arniePromises).catch((err) => {
+    console.warn(err);
+  });
 };
 
 module.exports = {
